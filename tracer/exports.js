3b3196684d100fad498fae3fea49c66c11f706ed
Sébastien Doeraene <sjrdoeraene@gmail.com>
2013-10-18 11:39:41 +0200

    Fix the "./run.sh opt" invocation.
    The exports.js scrips were using someArray.push()
    instead of someArray['push']().

--- tracer/exports.js
+++ tracer/exports.js
@@ -4,7 +4,7 @@
  */
 
 (function(ScalaJSBenchmarks) {
-  ScalaJSBenchmarks.push(function() {
+  ScalaJSBenchmarks['push'](function() {
     var benchmark = new ScalaJS.classes.benchmarks\ufe33tracer\ufe33Tracer();
     benchmark.report();
   });
